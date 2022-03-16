e9b619e51ca617b9f4a2c9313d9b24ac69b9bdec
Jerry <Jvictor940@gmail.com>
2022-03-16 12:10:12 -0400

    Arrow FFunctions

--- module.js
+++ module.js
@@ -1,11 +1,40 @@
-function quiz() {
-let myName= prompt('Jerry')
-let month= prompt('July')
-let food= prompt('Jerk Shrimp Linguine')
 
+const quiz = () => {
+    let myName= prompt('Jerry')
+    let month= prompt('July')
+    let food= prompt('Jerk Shrimp Linguine')
+    console.log('My name is', myName)
+    console.log('I was born in', month)
+    console.log('My favorite food is', food)
+}
 
-console.log('My name is', myName)
-console.log('I was born in', month)
-console.log('My favorite food is', food)
+const print = () => {
+    console.log('I love to print text!');
+}
 
+const functionA = () => {
+    console.log('FUNCTION A HAS BEEN CALLLED!')
+    functionB()
+}
+
+const functionB = () => {
+    console.log('FUNCTION B HAS BEEN CALLLED!')
+}
+//callback 
+const functionC = (f) => {
+    console.log('FUNCTION C HAS BEEN CALLED')
+    f()
+}
+
+const greet = (person) => {
+    console.log('HELLO', person)
+}
+
+const blender = (string) => {
+    return string.split('')
+}
+
+const square =(num, message) => {
+    console.log('Message:', message)
+    return num * num
 }
