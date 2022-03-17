4a54b323e04df09cdc3f9d26a699d5f1f74acb84
Jerry <Jvictor940@gmail.com>
2022-03-17 12:03:16 -0400

    added building, and apartments

---
+++ apartments/index.js
@@ -0,0 +1,40 @@
+let apt1 = {
+    bedrooms: 2,
+    sqft: 50, 
+    bathrooms: 3, 
+    windows: 0,
+    rent: 4500, 
+    unit: '1A'
+}
+let apt2 = {
+    bedrooms: 2,
+    sqft: 500, 
+    bathrooms: 3, 
+    windows: 0,
+    rent: 1500, 
+    unit: '1B'
+}
+let apt3 = {
+    bedrooms: 2,
+    sqft: 53, 
+    bathrooms: 3, 
+    windows: 2,
+    rent: 2400, 
+    unit: '1C'
+}
+
+
+let building = {
+    streetAdress: '11 Broadway, New York, NY, 11222',
+    laundry: false,
+    allowsPets: false, 
+    apartments: [apt1, apt2, apt3]
+
+}
+
+// bedrooms: ,
+//         sqft: ,
+//         bathrooms: ,
+//         windows: , 
+//         rent: ,
+//         unitNumber: ,
