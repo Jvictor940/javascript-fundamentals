86c8a47a038aaf2f1c3f72a0a4278ac3081e7658
Jerry <Jvictor940@gmail.com>
2022-03-17 16:20:18 -0400

    control flow lease

--- apartments/index.js
+++ apartments/index.js
@@ -55,8 +55,14 @@ let building = {
     laundry: false,
     allowsPets: false, 
     lease: function (apt, tenant) {
-        if(apt.tenants.length === apt.bedrooms) 
-        return `${apt.unit} is already full`
+        if(apt.tenants.length === apt.bedrooms) {
+            return `${apt.unit} is already full`
+        } 
+
+        if (this.allowsPets === false && tenant.pet) { 
+            return `${apt.unit} is available, but you must give ${tenant.pet} up for adoption!`
+        }
+            
         apt.tenants.push(tenant)
         console.log(tenant.name, "has rented out", apt.unit)
     },
