d90326b149fb7c20819199c8ff84cfa152573a41
Jerry <Jvictor940@gmail.com>
2022-03-18 15:22:51 -0400

    HTML CSS

---
+++ apartments/example_object.js
@@ -0,0 +1,54 @@
+const laptop = {
+    price: 1000,
+    brand: 'HP',
+    memory: '2GB',
+    color: 'pink'
+}
+
+const car = {
+    engine: 'V8',
+    doors: 2,
+    model: 'BMW',
+    price: 20000,
+    make: 'M4'
+}
+
+const company = {
+    name: 'Vittles',
+    ceo: 'Jerry Victor',
+    isTheBest: true,
+    publiclyTraded: false,
+}
+
+const room = {
+    windows: 0,
+    dimensions: '200X200',
+    closet: false,
+    isLegal: false,
+    pets: false,
+}
+
+const character = {
+    hairColor: 'blue',
+    height: "6'7",
+    race: 'elf',
+    gender: null,
+    strength: 'infinity',
+    dlc: false,
+    class: 'Mage',
+    cohort: 31422
+
+}
+
+const item = {
+    name: 'Excaliber',
+    power: 130
+}
+
+const ally = {
+    name: 'Geralt',
+    power: { name: 'Kamehameha', attackPower: 900 }
+}
+
+console.log(character)
+
